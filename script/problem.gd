77cb11def1d85a6dd73a40c286feeddb1e594070
LouisZCode <lgzg90@hotmail.com>
2025-02-16 15:20:01 +0100

    Made problems interactable
    Click E to interact with th eproblem

--- script/problem.gd
+++ script/problem.gd
@@ -2,25 +2,36 @@ extends Area2D
 
 @onready var prompt_label: RichTextLabel = $RichTextLabel
 
+
 var can_interact := false
 
 func _ready():
+	# Make sure label exists
+	if not prompt_label:
+		push_error("Label node not found in Problem scene")
+		return
+	prompt_label.text = "Press E"
 	prompt_label.hide()
+	# Debug print to confirm script is running
+	print("Problem initialized")
 
 func _process(_delta):
 	if can_interact and Input.is_action_just_pressed("interact"):
 		solve_problem()
 
 func _on_body_entered(body):
+	print("Body entered:", body.name)  # Debug print
 	if body.is_in_group("player"):
 		can_interact = true
 		prompt_label.show()
+		print("Player detected, showing prompt")  # Debug print
 
 func _on_body_exited(body):
+	print("Body exited:", body.name)  # Debug print
 	if body.is_in_group("player"):
 		can_interact = false
 		prompt_label.hide()
 
 func solve_problem():
-	# Play solve animation/sound here later
-	queue_free()  # For now, just remove the problem
+	print("Problem solved!")  # Debug print
+	queue_free()
