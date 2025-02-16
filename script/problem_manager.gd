ee41b899a38cc701ee10846e07615eec5e54166e
LouisZCode <lgzg90@hotmail.com>
2025-02-16 20:06:15 +0100

    add distractions to other rooms

--- script/problem_manager.gd
+++ script/problem_manager.gd
@@ -7,34 +7,54 @@ signal problem_solved(problem: Node)
 @export var spawn_interval_max := 8.0
 @export var max_simultaneous_problems := 3
 
-var problem_scene = preload("res://scenes/problem.tscn")  # Adjust path
-@onready var spawn_points = $"../Rooms/DiningRoom/ProblemSpawnPoints".get_children()
-@onready var date_manager = $"../DateManager"
-
+var problem_scene = preload("res://scenes/problem.tscn")
+@onready var spawn_points_root = $"../ProblemSpawnPoints"
 var active_problems := []
 
 func _ready():
+	print("ProblemManager ready")
 	start_spawn_timer()
 
 func start_spawn_timer():
-	var timer = get_tree().create_timer(randf_range(spawn_interval_min, spawn_interval_max))
+	var wait_time = randf_range(spawn_interval_min, spawn_interval_max)
+	print("Starting timer for ", wait_time, " seconds")
+	var timer = get_tree().create_timer(wait_time)
 	timer.timeout.connect(try_spawn_problem)
 
 func try_spawn_problem():
+	print("Trying to spawn problem. Active problems: ", active_problems.size())
 	if active_problems.size() < max_simultaneous_problems:
 		spawn_problem()
+	else:
+		print("Too many active problems")
 	start_spawn_timer()
 
+func get_all_available_spawn_points() -> Array:
+	var all_points = []
+	print("Checking for spawn points...")
+	
+	# Get all room spawn point groups
+	for room_points in spawn_points_root.get_children():
+		print("Checking spawn points in: ", room_points.name)
+		# Get individual spawn points (Marker2D nodes)
+		var points = room_points.get_children()
+		print("Found ", points.size(), " points in ", room_points.name)
+		all_points.append_array(points)
+	
+	# Filter out points that already have problems
+	var available = all_points.filter(func(point): return point.get_child_count() == 0)
+	print("Total available points: ", available.size())
+	return available
+
 func spawn_problem():
-	# Get available spawn points (those without problems)
-	var available_points = spawn_points.filter(func(point): 
-		return point.get_child_count() == 0
-	)
+	var available_points = get_all_available_spawn_points()
 	
 	if available_points.is_empty():
+		print("No available spawn points!")
 		return
 		
 	var spawn_point = available_points[randi() % available_points.size()]
+	print("Selected spawn point in: ", spawn_point.get_parent().name)
 	
 	var problem = problem_scene.instantiate()
 	spawn_point.add_child(problem)
@@ -46,7 +66,7 @@ func spawn_problem():
 	)
 	
 	emit_signal("problem_spawned", problem)
-	print("Problem spawned at: ", spawn_point.name)
+	print("Problem spawned at point in ", spawn_point.get_parent().name)
 
 func on_problem_solved(problem: Node):
 	active_problems.erase(problem)
