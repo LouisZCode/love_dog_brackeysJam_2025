d67769c93a74b67eb594fc4d305bcfa1199e246e
LouisZCode <lgzg90@hotmail.com>
2025-02-16 19:30:13 +0100

    Fix Camera and Location door spawns

---
+++ script/static_camera.gd
@@ -0,0 +1,27 @@
+extends Camera2D
+
+var room_positions = {
+	"DiningRoom": Vector2(0, -300),
+	"Kitchen": Vector2(-1100, -300),
+	"LivingRoom": Vector2(1100, -300),
+	"Basement": Vector2(0, 250)
+}
+
+@onready var rooms_node = get_node("../Rooms")
+
+func _ready():
+	# Make sure this camera is active
+	make_current()
+	enabled = true
+	
+	# Set initial position for dining room
+	global_position = room_positions["DiningRoom"]
+
+func _process(_delta):
+	update_camera_for_current_room()
+
+func update_camera_for_current_room():
+	for room in rooms_node.get_children():
+		if room.visible and room.name in room_positions:
+			global_position = room_positions[room.name]
+			break
