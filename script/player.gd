788f5a775d16796694cd974fade7345dc9cb5221
LouisZCode <lgzg90@hotmail.com>
2025-02-16 21:47:17 +0100

    created idle and run animation dog

--- script/player.gd
+++ script/player.gd
@@ -4,22 +4,15 @@ extends CharacterBody2D
 @export var run_speed := 600.0
 @export var acceleration := 1500.0
 @export var friction := 1000.0
-@export var debug_mode := true
 
-# Get the gravity from the project settings to be synced with RigidBody nodes
+@onready var animated_sprite = $AnimatedSprite2D
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
 enum DogState { IDLE, WALKING, RUNNING, FIXING }
 var current_state: DogState = DogState.IDLE
 var is_at_table := false
 var facing_right := true
 var current_noise := 0.0
-@export var noise_increase_rate := 2.0  # How fast noise increases while running
-@export var noise_decrease_rate := 1.0  # How fast noise decreases while walking/idle
-@export var max_noise := 100.0
-
-# Signal for noise level changes
-signal noise_level_changed(noise: float)
 
 func _physics_process(delta):
 	# Add the gravity
@@ -28,7 +21,7 @@ func _physics_process(delta):
 
 	# Get movement input
 	var direction = Input.get_axis("ui_left", "ui_right")
-	var is_running = Input.is_action_pressed("run")  # Space key by default
+	var is_running = Input.is_action_pressed("run")
 	
 	# Set speed based on running state
 	var target_speed = run_speed if is_running else walk_speed
@@ -38,68 +31,24 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, direction * target_speed, acceleration * delta)
 		current_state = DogState.RUNNING if is_running else DogState.WALKING
 		facing_right = direction > 0
-		
-		# Update noise level
-		if is_running:
-			update_noise(delta, true)
 	else:
 		velocity.x = move_toward(velocity.x, 0, friction * delta)
 		current_state = DogState.IDLE if is_on_floor() else current_state
-		update_noise(delta, false)
 
 	move_and_slide()
 	update_animation_state()
-	
-	if debug_mode:
-		print_debug_info()
-
-func update_noise(delta: float, is_running: bool) -> void:
-	if is_running:
-		current_noise = min(current_noise + noise_increase_rate * delta, max_noise)
-	else:
-		current_noise = max(current_noise - noise_decrease_rate * delta, 0.0)
-	
-	emit_signal("noise_level_changed", current_noise)
 
 func update_animation_state():
-	# This will be used later for animations
-	# For now we can just flip the sprite based on direction
-	if facing_right:
-		scale.x = abs(scale.x)
-	else:
-		scale.x = -abs(scale.x)
-
-func print_debug_info():
-	# Only print every 60 frames to avoid console spam
-	if Engine.get_physics_frames() % 60 == 0:
-		print("State: ", DogState.keys()[current_state])
-		print("At Table: ", is_at_table)
-		print("Noise Level: ", current_noise)
-		print("Velocity: ", velocity)
-
-func _on_table_area_entered(area):
-	if area.name == "TableArea":
-		is_at_table = true
-		if debug_mode:
-			print("Entered table area")
-
-func _on_table_area_exited(area):
-	if area.name == "TableArea":
-		is_at_table = false
-		if debug_mode:
-			print("Exited table area")
-
-func start_fixing():
-	current_state = DogState.FIXING
-	# Add fixing logic here later
-
-func stop_fixing():
-	current_state = DogState.IDLE
-	# Add stop fixing logic here later
-
-func get_noise_level() -> float:
-	return current_noise
-
-# This will be called from the ProblemManager when a problem is nearby
-func can_fix_problem() -> bool:
-	return current_state != DogState.FIXING and not is_at_table
+	# Update sprite direction
+	animated_sprite.flip_h = !facing_right
+	
+	# Update animation based on state
+	match current_state:
+		DogState.IDLE:
+			animated_sprite.play("idle")
+		DogState.WALKING, DogState.RUNNING:
+			animated_sprite.play("run")
+			# Optionally adjust animation speed based on running
+			animated_sprite.speed_scale = 1.5 if current_state == DogState.RUNNING else 1.0
+		DogState.FIXING:
+			animated_sprite.play("idle")  # Or a fixing animation if you have one
