731ca7e863275cbe98ba316e6edf66da6353a537
LouisZCode <lgzg90@hotmail.com>
2025-02-17 10:08:13 +0100

    Add Atic and Attic Door

---
+++ script/door_to_attic.gd
@@ -0,0 +1,55 @@
+extends Area2D
+
+@export var target_room: NodePath
+@export var spawn_position: Vector2
+@export_enum("Left", "Right") var door_side: String
+
+@onready var prompt_label = $RichTextLabel
+@onready var rooms_node = get_node("/root/Game/Rooms")
+
+# Static variable shared between all doors
+static var is_transitioning := false
+
+func _ready():
+	prompt_label.text = "[center]Press E[/center]"
+	prompt_label.hide()
+
+func _process(_delta):
+	# Only process if we're in the visible room
+	if not get_parent().visible:
+		return
+		
+	if not is_transitioning and overlaps_body(get_node("/root/Game/Player")) and Input.is_action_just_pressed("interact"):
+		transition_to_room()
+
+func _on_body_entered(body):
+	if body.is_in_group("player") and get_parent().visible:
+		prompt_label.show()
+
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		prompt_label.hide()
+
+func transition_to_room():
+	var target = get_node(target_room)
+	if target and not is_transitioning:
+		is_transitioning = true
+		
+		# Hide all rooms first
+		for room in rooms_node.get_children():
+			room.hide()
+		
+		# Show only the target room
+		target.show()
+		
+		# Move player to spawn position
+		var player = get_node("/root/Game/Player")
+		if player:
+			# Stop player movement
+			player.velocity = Vector2.ZERO
+			player.global_position = spawn_position
+			print("Transitioning to ", target.name, " at position: ", spawn_position)
+		
+		# Reset transition lock after a short delay
+		await get_tree().create_timer(0.5).timeout
+		is_transitioning = false
